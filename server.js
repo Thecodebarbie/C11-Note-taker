453fe9413735bbdefe637903366a7721d865d9ad
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-04-30 15:33:19 -0400

    Created an Express application instance and initiated the server to listen on the defined port. A console log message was added to indicate the server's listening status.

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require("express")
 const path = require("path")
 const PORT = process.env.PORT || 3001 //for deployment to heroku
 
+const app = express()
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log('app is listening at PORT: http://localhost:3001/'+PORT)
+})
