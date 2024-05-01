b6eed958dcef377c6db715845a78acf375330b5f
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-04-30 20:47:24 -0400

    Set up request body parsing middleware and basic HTML route
    Configured Express to use JSON and URL-encoded body parsing middleware to extract data from incoming requests accurately. Added a basic HTML route to serve the index.html file for any unmatched routes, allowing the application to respond appropriately to client requests.

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ const PORT = process.env.PORT || 3001 //for deployment to heroku
 const app = express()
 
 
+// extended  exact data from original after parsing.
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
 
+// api routes
 
+
+// html routes
+//http://localhost:3001/*
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/index/html'))
+})
 
 
 app.listen(PORT, ()=>{
     console.log('app is listening at PORT: http://localhost:3001/'+PORT)
 })
+
+
+
+
