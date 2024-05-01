c407c7d3a34f25449a7f7c3e98066cd43acb4a39
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-01 14:58:02 -0400

    Added API endpoints for reading and posting notes, and configured serving of HTML file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3001 //for deployment to heroku
 
 const app = express()
 
+const fs =  require("fs")
 
 // extended  exact data from original after parsing.
 app.use(express.json())
@@ -12,23 +13,33 @@ app.use(express.urlencoded({extended:true}))
 
 
 // api routes
+app.get("/api/notes", (req,res)=>{
+    fs.readFile("./db/db.json","utf8",(err,data)=>{
+        const newData = JSON.parse(data)
+        res.json(newData)
+    })
+})
+
+
+app.post("/api/notes", (req,res)=>{
+
+})
+
+app.get("/notes", (req,res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 
 
 // html routes
 //http://localhost:3001/*
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname, './public/index/html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.use("/notes",(req,res)=>{
-    res.sendFile(err=>{
-        path.join(__dirname, "./notes.html")
-    })
-})
 
 
 app.listen(PORT, ()=>{
-    console.log('app is listening at PORT: http://localhost:3001/'+PORT)
+    console.log('app is listening at PORT: http://localhost:'+PORT)
 })
 
 
